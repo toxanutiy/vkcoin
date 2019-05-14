5517f81f442586bd322f4c3a0c545026f5b60d52
toxanutiy <toxanutiy@gmail.com>
2019-05-14 10:31:29 +0300

    Updates

---
+++ precache-manifest.26c5e4428c6bd344b8fea7dab056ea43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5525c4fcb1586e5f755",
+    "url": "/vkcoin/static/css/main.ba310526.chunk.css"
+  },
+  {
+    "revision": "e5525c4fcb1586e5f755",
+    "url": "/vkcoin/static/js/main.e5525c4f.chunk.js"
+  },
+  {
+    "revision": "9a36d6dd15a08f9ddc62",
+    "url": "/vkcoin/static/js/runtime~main.9a36d6dd.js"
+  },
+  {
+    "revision": "1549efb2dda15d082cfc",
+    "url": "/vkcoin/static/css/2.fc8be980.chunk.css"
+  },
+  {
+    "revision": "1549efb2dda15d082cfc",
+    "url": "/vkcoin/static/js/2.1549efb2.chunk.js"
+  },
+  {
+    "revision": "bac228175099b84ef1d0db761c7099f1",
+    "url": "/vkcoin/index.html"
+  }
+];
