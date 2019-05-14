528459b6c1117ddec751ac42b9f5bdf0b56f599a
toxanutiy <toxanutiy@gmail.com>
2019-05-14 10:20:14 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import connect from '@vkontakte/vkui-connect';
+import App from './App';
+
+connect.send('VKWebAppInit', {});
+
+ReactDOM.render(<App />, document.getElementById('root'));
